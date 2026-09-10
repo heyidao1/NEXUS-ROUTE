a7e3a1d7d585b93a8e6c5a2bd96c676b629872e3
heyidao1 <fff223343@gmail.com>
2026-09-10 20:37:59 +0800

    Run public asset and endpoint audits before triage

--- run_safe_hunter.ps1
+++ run_safe_hunter.ps1
@@ -16,6 +16,8 @@ try {
   Set-Location $root
   $env:PYTHONUTF8 = '1'
   python .\bounty\safe_hunter.py *>&1 | Tee-Object -FilePath $log
+  python .\bounty\public_asset_audit.py *>&1 | Tee-Object -FilePath $log -Append
+  python .\bounty\endpoint_audit.py *>&1 | Tee-Object -FilePath $log -Append
   python .\bounty\triage_queue.py *>&1 | Tee-Object -FilePath $log -Append
   Get-Date -Format o | Set-Content -Encoding UTF8 (Join-Path $state 'last-success.txt')
 } catch {
